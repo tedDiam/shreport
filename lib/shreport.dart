75ba74d92b962e1657e91f4707ccbf454470fedf
sergediame <serge.diame@paynah.com>
2025-02-22 22:57:00 +0000

    export additional modules for shreport
    - Added exports for `shreport_wrapper.dart`, `shreport_jira.dart`, `issue_tracker_config.dart`, and `shake_feedback.dart`
    - Included `feedback` package export

--- lib/shreport.dart
+++ lib/shreport.dart
@@ -1,5 +1,7 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+library shreport;
+
+export 'package:feedback/feedback.dart';
+export 'src/shreport_wrapper.dart';
+export 'src/shreport_jira.dart';
+export 'src/issue_tracker_config.dart';
+export 'src/shake_feedback.dart';
